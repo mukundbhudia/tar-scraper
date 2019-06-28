4ebfd84f194960a2a7a25adb66cdc8eb92d99777
Mukund Bhudia <mukund_bhudia@hotmail.com>
2019-06-28 20:11:12 +0930

    Indentation fixes and comment removal

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ process.argv.shift()
 process.argv.shift()
 
 const url = process.argv.join(" ")
-// const url = 'https://www.tripadvisor.com.sg/Attraction_Review-g293891-d16813813-Reviews-Ankit_Treks-Pokhara_Gandaki_Zone_Western_Region.html';
-// const url = 'https://www.tripadvisor.com.sg/Restaurant_Review-g293891-d8508841-Reviews-Rosemary_Kitchen-Pokhara_Gandaki_Zone_Western_Region.html'
+
 console.log('Finding reviews from: ' + url + '\n')
 let numberOfReviewsFound = 0;
 
@@ -23,8 +22,8 @@ rp(url)
 
     reviewPageUrls.push(url);
     for (let i = 1; i < numberOfReviewPages; i++) {
-        let reviewUrl = url.replace("-Reviews-", "-Reviews-or" + i + "0-");
-        reviewPageUrls.push(reviewUrl);
+      let reviewUrl = url.replace("-Reviews-", "-Reviews-or" + i + "0-");
+      reviewPageUrls.push(reviewUrl);
     }
 
     console.log('Found ' + reviewPageUrls.length + ' review pages with ' + numberOfReviewsFound + ' reviews...\n');
@@ -36,21 +35,21 @@ rp(url)
       );
     })
     .then(function(reviews) {
-        let merged = [].concat.apply([], reviews);
-        console.log('There are ' + numberOfReviewsFound + ' reviews on the page and ' + merged.length + ' have been scraped');
-        if (numberOfReviewsFound != merged.length) {
-          return console.log('Error: There is a mismatch between the number of reviews on the page and the number scraped');
-        } else {
-          var jsonContent = JSON.stringify(merged);
-          
-          fs.writeFile("out/output.json", jsonContent, 'utf8', function (err) {
-              if (err) {
-                  console.log("An error occurred while writing JSON Object to File.");
-                  return console.log(err);
-              }          
-              console.log("JSON file has been saved.");
-          });
-        }
+      let merged = [].concat.apply([], reviews);
+      console.log('There are ' + numberOfReviewsFound + ' reviews on the page and ' + merged.length + ' have been scraped');
+      if (numberOfReviewsFound != merged.length) {
+        return console.log('Error: There is a mismatch between the number of reviews on the page and the number scraped');
+      } else {
+        var jsonContent = JSON.stringify(merged);
+        
+        fs.writeFile("out/output.json", jsonContent, 'utf8', function (err) {
+          if (err) {
+            console.log("An error occurred while writing JSON Object to File.");
+            return console.log(err);
+          }          
+          console.log("JSON file has been saved.");
+        });
+      }
     })
   .catch(function(err){
     console.error(err);
--- reviewScraper.js
+++ reviewScraper.js
@@ -2,32 +2,32 @@ const rp = require('request-promise');
 const $ = require('cheerio');
 
 const reviewParse = function(url) {
-    return rp(url)
+  return rp(url)
     .then(function(html){
       const reviewContainer = $('div.review-container', html);
       const reviewsDiv = [];
       console.log('Processing and collecting: ' + reviewContainer.length + ' reviews')
 
       for (let i = 0; i < reviewContainer.length; i++) {
-          const reviewTitle = $('span.noQuotes', reviewContainer[i])[0].children[0].data
-          const reviewText = $('p.partial_entry', reviewContainer[i])[0].children[0].data
-          const reviewUserName = $('div.info_text > div', reviewContainer[i])[0].children[0].data
-          const reviewDate = $('span.ratingDate', reviewContainer[i])[0].children[0].parent.attribs.title
-          const reviewUserAvatar = $('div.ui_avatar.resp > img.basicImg', reviewContainer[i])[0].attribs.src
-          const reviewRating = $('span.ui_bubble_rating', reviewContainer[i])[0].attribs.class
-          const reviewId = $('div.reviewSelector', reviewContainer[i])[0].attribs['data-reviewid']
+        const reviewTitle = $('span.noQuotes', reviewContainer[i])[0].children[0].data
+        const reviewText = $('p.partial_entry', reviewContainer[i])[0].children[0].data
+        const reviewUserName = $('div.info_text > div', reviewContainer[i])[0].children[0].data
+        const reviewDate = $('span.ratingDate', reviewContainer[i])[0].children[0].parent.attribs.title
+        const reviewUserAvatar = $('div.ui_avatar.resp > img.basicImg', reviewContainer[i])[0].attribs.src
+        const reviewRating = $('span.ui_bubble_rating', reviewContainer[i])[0].attribs.class
+        const reviewId = $('div.reviewSelector', reviewContainer[i])[0].attribs['data-reviewid']
 
-          const outOfFiveRating = (reviewRating.match(/\d+/)[0]) / 10
+        const outOfFiveRating = (reviewRating.match(/\d+/)[0]) / 10
 
-          reviewsDiv.push({
-              'reviewId': reviewId,
-              'reviewTitle': reviewTitle,
-              'reviewText': reviewText,
-              'reviewUserName': reviewUserName,
-              'reviewDate': reviewDate,
-              'reviewUserAvatar': reviewUserAvatar,
-              'outOfFiveRating': outOfFiveRating
-          });
+        reviewsDiv.push({
+            'reviewId': reviewId,
+            'reviewTitle': reviewTitle,
+            'reviewText': reviewText,
+            'reviewUserName': reviewUserName,
+            'reviewDate': reviewDate,
+            'reviewUserAvatar': reviewUserAvatar,
+            'outOfFiveRating': outOfFiveRating
+        });
       }
       return reviewsDiv;
     })
